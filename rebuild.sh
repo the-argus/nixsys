efa2a402436bba1dbe42ec83ca1e2099f6441756
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-25 16:56:53 -0500

    typo 2.0

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-pushd SCRIPT_DIR
+pushd $SCRIPT_DIR
 sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
 popd
