b7803baff1fa7d13166f102b95614b418b4ff340
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-25 17:02:07 -0500

    fixed rebuild.sh to support hardware-configuration.nix

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,13 @@
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 
-pushd $SCRIPT_DIR
+# check if hardware config exists
+HARDWARE_DIR=$SCRIPT_DIR/system/hardware-configuration.nix
+if [ ! -e $HARDWARE_DIR ]; then
+    echo "copy your hardware-configuration.nix to" $HARDWARE_DIR
+    exit
+fi
+
+    pushd $SCRIPT_DIR
 sudo nixos-rebuild switch -I nixos-config=./system/configuration.nix
 popd
