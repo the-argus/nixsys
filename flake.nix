09e092d7eba14903b0fbb337e4dac26a8b218bc9
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-26 10:12:42 -0500

    this stuff is out of date

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "the-argus nixos system configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    # master so its unstable
+    home-manager.url = "github:nix-community/home-manager/master";
+    # home manager use out nixpkgs and not its own
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        # hostname (evil)
+        evil = lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            ./system/configuration.nix
+          ];
+        };
+      };
+    };
+}
---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  name = "nixosbuildshell";
+  nativeBuildInputs = with pkgs; [
+    git
+    git-crypt
+    nixFlakes
+  ];
+
+  shellHook = ''
+    alias rebuild="sudo nixos-rebuild switch --flake .#"
+    alias update="nix flake update --recreate-lock-file"
+
+      echo "You can apply this flake to your system with \"rebuild\""
+      echo "And update it with \"update\""
+      PATH=${pkgs.writeShellScriptBin "nix" ''
+        ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+      ''}/bin:$PATH
+    '';
+  }
