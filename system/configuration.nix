e21397e710e4b65f4c8398ec5c764e04e413c198
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-26 22:10:22 -0500

    moved to /etc hardware configuration for portability

--- system/configuration.nix
+++ system/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
