58fdefe4f5c6b5c3448c7e7828de6ca55b8eae47
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-05-28 13:07:22 -0500

    updated stateversion and usergroup

--- system/configuration.nix
+++ system/configuration.nix
@@ -53,6 +53,8 @@
     extraGroups = [
       "wheel"
       "video"
+      "systemd-network"
+      "networkmanager"
     ];
   };
 
@@ -162,7 +164,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 
 }
 
