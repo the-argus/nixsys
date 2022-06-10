98d2b60ec311c655cabf6a0ef76df7c2d51e4a6a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-06-10 15:54:01 -0500

    reverted graphics packages

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -33,15 +33,10 @@
   hardware.opengl = {
     extraPackages = with pkgs; [
       intel-media-driver
-      libGL
-      libGLU
-      python310Packages.glad
-      python310Packages.glfw
-      freeglut
     ];
-    extraPackages32 = with pkgs.pkgsi686Linux;
-      [ libva vaapiIntel libvdpau-va-gl vaapiVdpau ]
-      ++ lib.optionals config.services.pipewire.enable [ pipewire ];
+    # extraPackages32 = with pkgs.pkgsi686Linux;
+    #   [ libva vaapiIntel libvdpau-va-gl vaapiVdpau ]
+    #   ++ lib.optionals config.services.pipewire.enable [ pipewire ];
   };
 
   #	services.xserver.videoDrivers = [ "intel" ];
