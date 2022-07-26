f9637231fc94d5bf0e9266d5565821db429d8e14
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-07-26 08:37:57 -0500

    moved discord.desktop to nix configuration

--- user/argus/local/default.nix
+++ user/argus/local/default.nix
@@ -1,13 +1,20 @@
 { ... }: {
   home.file = {
-    ".local/share/applications" = {
-      source = ./applications;
-      recursive = true;
-    };
-
     ".local/bin" = {
       source = ./bin;
       recursive = true;
+    };
+  };
+
+  xdg.desktopEntries = {
+    discord = {
+      name = "Discord (Performance)";
+      comment = "All-in-one voice and text chat for gamers that's free, secure, and works on both your desktop and phone.";
+      genericName = "Internet Messenger";
+      exec = ''flatpak run com.discordapp.Discord --command="discord --no-sandbox --enable-accelerated-mjpeg-decode --enable-accelerated-video --ignore-gpu-blacklist --enable-native-gpu-memory-buffers --enable-gpu-rasterization"'';
+      icon = "discord";
+      type = "Application";
+      categories = [ "Network" "InstantMessaging" ];
     };
   };
 }
