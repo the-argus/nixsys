003ec0c2d145d6875c307ab061bae5b38f47b612
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-08 23:01:40 -0500

    spicetify finally done

--- user/argus/spicetify.nix
+++ user/argus/spicetify.nix
@@ -11,80 +11,71 @@
   # configure spicetify :)
   programs.spicetify =
     let
-      # av = pkgs.fetchFromGitHub {
-      #   owner = "amanharwara";
-      #   repo = "spicetify-autoVolume";
-      #   rev = "d7f7962724b567a8409ef2898602f2c57abddf5a";
-      #   sha256 = "1pnya2j336f847h3vgiprdys4pl0i61ivbii1wyb7yx3wscq7ass";
-      # };
-
-      hidePodcasts = pkgs.fetchgit {
-        url = "https://github.com/theRealPadster/spicetify-hide-podcasts";
-        rev = "cfda4ce0c3397b0ec38a971af4ff06daba71964d";
-        sha256 = "146bz9v94dk699bshbc21yq4y5yc38lq2kkv7w3sjk4x510i0v3q";
-      };
-
-      history = pkgs.fetchgit {
-        url = "https://github.com/einzigartigerName/spicetify-history";
-        rev = "577e34f364127f18d917d2fe2e8c8f2a1af9f6ae";
-        sha256 = "0fv5fb6k9zc446a1lznhmd68m47sil5pqabv4dmrqk6cvfhba49r";
-      };
-
-      genre = pkgs.fetchgit {
-        url = "https://github.com/Shinyhero36/Spicetify-Genre";
-        rev = "4ab66852825525869ef5ced5747e7e84ddd0a8bb";
-        sha256 = "09b69dcknqvj9nc5ayfqcdg63vc5yshn0wa23gyachzicwalq30m";
-      };
-
-      lastfm = pkgs.fetchgit {
-        url = "https://github.com/LucasBares/spicetify-last-fm";
-        rev = "0f905b49362ea810b6247ac1950a2951dd35632e";
-        sha256 = "1b0l2g5cyjj1nclw1ff7as9q94606mkq1k8l2s34zzdsx3m2zv81";
-      };
-
-      localFiles = pkgs.fetchgit {
-        url = "https://github.com/hroland/spicetify-show-local-files/";
-        rev = "1bfd2fc80385b21ed6dd207b00a371065e53042e";
-        sha256 = "01gy16b69glqcalz1wm8kr5wsh94i419qx4nfmsavm4rcvcr3qlx";
+      officialThemesOLD = pkgs.fetchgit {
+        url = "https://github.com/spicetify/spicetify-themes";
+        rev = "c2751b48ff9693867193fe65695a585e3c2e2133";
+        sha256 = "0rbqaxvyfz2vvv3iqik5rpsa3aics5a7232167rmyvv54m475agk";
       };
     in
-    {
-      # use unstable channel for these because old versions tend to inject improperly
-      # spotifyPackage = unstable.spotify // { version = unstable.spotify-unwrapped.version; };
-      spotifyPackage = unstable.spotify-unwrapped;
-      # spicetifyPackage = unstable.spicetify-cli;
-      spicetifyPackage = import ../../packages/spicetify-cli-2.9.9.nix { inherit pkgs; };
-      enable = true;
-      theme = "Dribbblish";
-      colorScheme = "rosepine";
-      enabledCustomApps = [
-        # "new-releases"
-        # {
-        #     name = "localFiles";
-        #     src = localFiles;
-        #     appendName = false;
-        # }
+  {
+    # use unstable channel for these because old versions tend to inject improperly
+    # spotifyPackage = unstable.spotify // { version = unstable.spotify-unwrapped.version; };
+    spotifyPackage = unstable.spotify-unwrapped;
+    # spicetifyPackage = unstable.spicetify-cli;
+    spicetifyPackage = import ../../packages/spicetify-cli-2.9.9.nix { inherit pkgs; };
+    enable = true;
+    theme = {
+      name = "Dribbblish";
+      src = officialThemesOLD;
+      requiredExtensions = [
+        {
+          filename = "dribbblish.js";
+          src = "${officialThemesOLD}/Dribbblish";
+        }
       ];
-      enabledExtensions = [
-        {
-          src = hidePodcasts;
-          filename = "hidePodcasts.js";
-        }
-        {
-          src = history;
-          filename = "historyShortcut.js";
-        }
-        {
-          src = genre;
-          filename = "genre.js";
-        }
-        {
-          src = "${lastfm}/src";
-          filename = "lastfm.js";
-        }
-        "dribbblish.js"
-        "fullAppDisplay.js"
-        "shuffle+.js"
-      ];
+      patches = {
+        "xpui.js_find_8008" = ",(\\w+=)32,";
+        "xpui.js_repl_8008" = ",$\{1}56,";
+      };
+      injectCss = true;
+      replaceColors = true;
+      overwriteAssets = true;
+      appendName = true;
+      sidebarConfig = true;
     };
+    colorScheme = "custom";
+
+    customColorScheme = {
+      text = "ebbcba";
+      subtext = "F0F0F0";
+      sidebar-text = "e0def4";
+      main = "191724";
+      sidebar = "2a2837";
+      player = "191724";
+      card = "191724";
+      shadow = "1f1d2e";
+      selected-row = "797979";
+      button = "31748f";
+      button-active = "31748f";
+      button-disabled = "555169";
+      tab-active = "ebbcba";
+      notification = "1db954";
+      notification-error = "eb6f92";
+      misc = "6e6a86";
+    };
+
+    enabledCustomApps = with spicetify-nix.pkgs.apps; [
+      # new-releases
+      # localFiles
+    ];
+    enabledExtensions = with spicetify-nix.pkgs.extensions; [
+      "playlistIcons.js"
+      "lastfm.js"
+      "genre.js"
+      "historyShortcut.js"
+      "hidePodcasts.js"
+      "fullAppDisplay.js"
+      "shuffle+.js"
+    ];
+  };
 }
