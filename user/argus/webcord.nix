c4b84f42eaa3d7c8109785e7c6e0ce75e6276642
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-14 01:00:55 -0500

    source not src

--- user/argus/webcord.nix
+++ user/argus/webcord.nix
@@ -2,7 +2,7 @@
 {
   home.file = {
     ".config/WebCord/Themes" = {
-      src = pkgs.fetchgit {
+      source = pkgs.fetchgit {
         url = "https://github.com/DiscordStyles/SoftX";
         rev = "ef11558a47b3ce6b590e8d8ea47e34fc1de32d9c";
         sha256 = "1b8k640jymd6hcqr9930i6m98ihxcya18biaazsp5hcvkxh25ac6";
