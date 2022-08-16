5fc934b78221e9546a2b63e29075157b36c96515
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-15 23:28:12 -0500

    neovim follows color palette

--- user/argus/config/nvim/default.nix
+++ user/argus/config/nvim/default.nix
@@ -1,36 +1,45 @@
 { pkgs, nvim-config, ... }: {
-  home.file = {
-    ".config/nvim" = {
-      source = nvim-config;
+  home.file = with (pkgs.callPackage ../../themes.nix { }).scheme; {
+    ".config/nvim" = 
+      let
+        text = ''
+          local palette = {
+              base = '#${bg}00',
+              surface = '#${altbg2}',
+              overlay = '#${altfg2}',
+              muted = '#${black}',
+              subtle = '#${altfg3}',
+              text = '#${white}',
+              love = '#${red}',
+              gold = '#${yellow}',
+              rose = '#${cyan}',
+              pine = '#${blue}',
+              foam = '#${green}',
+              iris = '#${magenta}',
+              highlight_low = '#${altbg}',
+              highlight_med = '#${altbg3}',
+              highlight_high = '#${altbg3}',
+              opacity = 0.1,
+              none = 'NONE',
+          }
+
+          return palette
+        '';
+        override = builtins.toFile "color-override.lua" text;
+
+        overridenConfig = pkgs.stdenv.mkDerivation {
+            name = "overriden-neovim-config";
+            src = nvim-config;
+            buildPhase = ''
+                cp ${override} lua/color-override.lua
+                ${pkgs.coreutils-full}/bin/chmod -R a+wr .
+            '';
+            installPhase = "cp -r . $out";
+        };
+      in
+    {
+      source = overridenConfig;
       recursive = true;
     };
-
-    ".config/nvim/lua/color-override.lua".text = 
-    let
-        p = (pkgs.callPackage ../../themes.nix {}).scheme;
-    in with p;
-    ''
-      local palette = {
-          base = '#${p.bg}00',
-          surface = '#${altbg2}',
-          overlay = '#${altfg2}',
-          muted = '#${black}',
-          subtle = '#${altfg3}',
-          text = '#${white}',
-          love = '#${red}',
-          gold = '#${yellow}',
-          rose = '#${cyan}',
-          pine = '#${blue}',
-          foam = '#${green}',
-          iris = '#${magenta}',
-          highlight_low = '#${altbg}',
-          highlight_med = '#${altbg3}',
-          highlight_high = '#${altbg3}',
-          opacity = 0.1,
-          none = 'NONE',
-      }
-
-      return palette
-    '';
   };
 }
