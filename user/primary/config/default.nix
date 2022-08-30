247a7a1e24d47ae3b503922201834cae459601e8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-30 18:00:39 -0400

    xinitrc handled by nix

--- user/primary/config/default.nix
+++ user/primary/config/default.nix
@@ -1,9 +1,47 @@
-{ ... }: {
-    imports = [
-        ./nvim
-        ./ranger
-        ./neofetch
-        ./qtile
-        ./rofi
-    ];
+{ pkgs, ... }: {
+  imports = [
+    ./nvim
+    ./ranger
+    ./neofetch
+    ./qtile
+    ./rofi
+  ];
+
+  home.file.".xinitrc".text = ''
+    #!/bin/sh
+
+    userresources=$HOME/.Xresources
+    usermodmap=$HOME/.Xmodmap
+    sysresources=${pkgs.xorg.xinit}/etc/X11/xinit/.Xresources
+    sysmodmap=${pkgs.xorg.xinit}/etc/X11/xinit/.Xmodmap
+
+    # merge in defaults and keymaps
+
+    if [ -f $sysresources ]; then
+        xrdb -merge $sysresources
+    fi
+
+    if [ -f $sysmodmap ]; then
+        xmodmap $sysmodmap
+    fi
+
+    if [ -f "$userresources" ]; then
+        xrdb -merge "$userresources"
+    fi
+
+    if [ -f "$usermodmap" ]; then
+        xmodmap "$usermodmap"
+    fi
+
+    # start some nice programs
+
+    if [ -d ${pkgs.xorg.xinit}/etc/X11/xinit/xinitrc.d ] ; then
+     for f in ${pkgs.xorg.xinit}/etc/X11/xinit/xinitrc.d/?*.sh ; do
+      [ -x "$f" ] && . "$f"
+     done
+     unset f
+    fi
+
+    PYTHONDONTWRITEBYTECODE="yes" GTK_USE_PORTAL=0 exec qtile start
+  '';
 }
