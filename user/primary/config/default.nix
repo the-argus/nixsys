e485adc6947ac0db6d4af8e43f07d6f8a03c48c8
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-01 08:34:28 -0400

    dbus fix for gtk apps maybe

--- user/primary/config/default.nix
+++ user/primary/config/default.nix
@@ -41,6 +41,9 @@
      done
      unset f
     fi
+    
+    # supplied by pkgs.dbus
+    dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY
 
     PYTHONDONTWRITEBYTECODE="yes" GTK_USE_PORTAL=0 exec qtile start
   '';
