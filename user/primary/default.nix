d826728bf49103f36a085bd1bf4fe8bbf653fcd7
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-08-31 23:48:43 -0400

    firefox default browser

--- user/primary/default.nix
+++ user/primary/default.nix
@@ -50,9 +50,15 @@
     enable = true;
     associations.added = {
       "inode/directory" = [ "ranger.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
     };
     defaultApplications = {
       "inode/directory" = [ "ranger.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
     };
   };
 
