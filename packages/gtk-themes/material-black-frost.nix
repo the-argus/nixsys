c7b7e83bbab80070344efd009d6cbe8b8683a82d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-07 22:34:06 -0400

    fix home manager path collision

--- packages/gtk-themes/material-black-frost.nix
+++ packages/gtk-themes/material-black-frost.nix
@@ -1,6 +1,16 @@
-{ pkgs, ... }:
-pkgs.fetchgit {
-  url = "https://github.com/rtlewis88/rtl88-Themes";
-  rev = "c8d57d6ee1ce8156b2a873bb064b493e9421cf61";
-  sha256 = "0vk1hslfglqk8vpp4h403hzxkj447y5plf04swk5h5jcz21ik265";
+{ pkgs, stdenv, coreutils-full, ... }:
+stdenv.mkDerivation {
+  name = "MaterialBlackFrost-Theme";
+  src = pkgs.fetchgit {
+    url = "https://github.com/rtlewis88/rtl88-Themes";
+    rev = "c8d57d6ee1ce8156b2a873bb064b493e9421cf61";
+    sha256 = "0vk1hslfglqk8vpp4h403hzxkj447y5plf04swk5h5jcz21ik265";
+  };
+  installPhase = ''
+    mkdir $out
+    cp $src/* $out
+    ${coreutils-full}/bin/chmod w+r $out/*
+    # README causes a collision in home manager path for some reason
+    rm $out/README.md
+  '';
 }
