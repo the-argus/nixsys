3843ffe876d2e5ff48de4537d111f505b59423f5
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-07 22:26:17 -0400

    actually call the themes module

--- user/primary/themes.nix
+++ user/primary/themes.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 # if settings had a theme, it gets added to pkgs set in flake.nix
 let
-  default = (import ../../modules/color/themes.nix).defaultTheme;
+  themes = (pkgs.callPackage ../../modules/color/themes.nix { });
+  default = themes.defaultTheme;
 in
 (if builtins.hasAttr "theme" pkgs then
   (
     if builtins.typeOf pkgs.theme == "string" then
-      (import ../../modules/color/themes.nix).${pkgs.theme}
+      themes.${pkgs.theme}
     else if builtins.typeOf pkgs.theme == "set" then
       pkgs.theme
     else
