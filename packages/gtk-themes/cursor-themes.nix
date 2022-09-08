579fe0a1d0dd8208849a62530d68fa4bdfe9171d
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-07 22:29:18 -0400

    fix hash

--- packages/gtk-themes/cursor-themes.nix
+++ packages/gtk-themes/cursor-themes.nix
@@ -8,7 +8,7 @@ in
     name = "BreezeXBlack";
     package = builtins.fetchTarball {
       url = "https://github.com/ful1e5/BreezeX_Cursor/releases/download/v1.0.3/BreezeX-Black.tar.gz";
-      sha256 = "0s73lg93wb6rdvap6cs11wq6h3fw18ziasxrhgagklf35jrq62k4";
+      sha256 = "sha256:10sj6fmpxs8509lpjz7pj0iyahi29fn206m2lr5sv1ks9g6hnav4";
     };
   };
   googleDotBlack = {
