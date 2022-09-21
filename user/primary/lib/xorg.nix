52612bcaca1da1ddbbe7fcacbd8d419aa838ce00
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-21 15:33:35 -0400

    reformatted functions in lib/xorg.nix

--- user/primary/lib/xorg.nix
+++ user/primary/lib/xorg.nix
@@ -14,22 +14,19 @@
   execShell = path: "${path} &";
   execI3 = path: "--no-startup-id ${path}";
 in rec {
-  bluetoothAutostart = {execFunc ? execShell}:
-    map execFunc [
-      # laptop has bluetooth and wireless
-      "${pkgs.blueman}/bin/blueman-applet"
-    ];
+  bluetoothAutostart = [
+    # laptop has bluetooth and wireless
+    "${pkgs.blueman}/bin/blueman-applet"
+  ];
 
-  wirelessAutostart = {execFunc ? execShell}:
-    map execFunc [
-      "${pkgs.networkmanagerapplet}/bin/nm-applet"
-    ];
+  wirelessAutostart = [
+    "${pkgs.networkmanagerapplet}/bin/nm-applet"
+  ];
 
-  mouseAutostart = {execFunc ? execShell}:
-    map execFunc [
-      # i use a mouse on my pc, so theres middleclick
-      "${pkgs.xmousepasteblock}/bin/xmousepasteblock"
-    ];
+  mouseAutostart = [
+    # i use a mouse on my pc, so theres middleclick
+    "${pkgs.xmousepasteblock}/bin/xmousepasteblock"
+  ];
 
   mkAutoStart = {
     isI3 ? false,
@@ -40,22 +37,16 @@ in rec {
       then execI3
       else execShell;
   in
-    map execFunc [
-      "${pkgs.dunst}/bin/dunst"
-      "${pkgs.xfce.xfce4-clipman-plugin}/bin/xfce4-clipman"
-      "${pkgs.xclip}/bin/xclip"
-      "picom --config ${picomConfigLocation}"
+    map execFunc ([
+        "${pkgs.dunst}/bin/dunst"
+        "${pkgs.xfce.xfce4-clipman-plugin}/bin/xfce4-clipman"
+        "${pkgs.xclip}/bin/xclip"
+        "picom --config ${picomConfigLocation}"
 
-      # restore feh wallpaper
-      "$HOME/.fehbg"
-    ]
-    ++ (optionalList settings.usesWireless (wirelessAutostart {
-      inherit execFunc;
-    }))
-    ++ (optionalList settings.usesBluetooth (bluetoothAutostart {
-      inherit execFunc;
-    }))
-    ++ (optionalList settings.usesMouse (mouseAutostart {
-      inherit execFunc;
-    }));
+        # restore feh wallpaper
+        "$HOME/.fehbg"
+      ]
+      ++ (optionalList settings.usesWireless wirelessAutostart)
+      ++ (optionalList settings.usesBluetooth bluetoothAutostart)
+      ++ (optionalList settings.usesMouse mouseAutostart));
 }
