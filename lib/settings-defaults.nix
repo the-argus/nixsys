2cbdc548358782ce5c129cbac8a9dfcf76aa7e2f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-09-27 16:41:56 -0500

    fix error with default config

--- lib/settings-defaults.nix
+++ lib/settings-defaults.nix
@@ -51,7 +51,7 @@
     #"steam"
   ]; # will be evaluated later
   additionalOverlays = [];
-  hardwareConfiguration = [./system/hardware];
+  hardwareConfiguration = [../system/hardware];
   usesWireless = true; # install and autostart nm-applet
   usesBluetooth = true; # install and autostart blueman applet
   usesMouse = false; # enables xmousepasteblock for middle click
