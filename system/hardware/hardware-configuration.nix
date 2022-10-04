08d076ad25b8492cfa04be6aa4a8140322478407
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-04 16:08:30 -0400

    make generic config options more generic

--- system/hardware/hardware-configuration.nix
+++ system/hardware/hardware-configuration.nix
@@ -20,21 +20,21 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/444dd843-a3b1-4e59-9d47-c62cfab94d8b";
+    device = "/dev/disk/by-label/NIXROOT";
     fsType = "ext4";
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/3b8082af-c41c-4b45-b4f4-5ece2a003373";
+    device = "/dev/disk/by-label/NIXHOME";
     fsType = "btrfs";
   };
 
   fileSystems."/efi" = {
-    device = "/dev/disk/by-uuid/F230-06EA";
+    device = "/dev/disk/by-label/WINBOOT";
     fsType = "vfat";
   };
 
-  swapDevices = [{device = "/dev/disk/by-uuid/0d554a74-2d3c-4bd4-a5db-1fc7062485f9";}];
+  swapDevices = [{device = "/dev/disk/by-label/SWAP";}];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
