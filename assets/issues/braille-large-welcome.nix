c21e25b323db54708fe1af8ad10f79661d9b9419
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-05 23:14:44 -0400

    use proper format for /etc/issue colors

--- assets/issues/braille-large-welcome.nix
+++ assets/issues/braille-large-welcome.nix
@@ -1,8 +1,8 @@
 {username}: let
-  color1 = "\\033[32m";
-  color2 = "\\033[34m";
-  color3 = "\\033[33m";
-  color4 = "\\033[35m";
+  color1 = "^[[1;32m";
+  color2 = "^[[1;34m";
+  color3 = "^[[1;33m";
+  color4 = "^[[1;35m";
 in "
   ${color1}          ::::.    ${color2}':::::     ::::'
   ${color1}          ':::::    ${color2}':::::.  ::::'
