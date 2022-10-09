908bfbf14130fd75956088d065c74498299589b1
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-09 10:44:33 -0400

    use banner.palette instead of system.theme.scheme

--- user/primary/config/nvim/default.nix
+++ user/primary/config/nvim/default.nix
@@ -4,46 +4,44 @@
   config,
   ...
 }: {
-  home.file = with config.system.theme.scheme; {
-    ".config/nvim" = let
-      text = ''
-        local palette = {
-            base = '#${base00}00',
-            surface = '#${base02}',
-            overlay = '#${base02}',
-            muted = '#${base00}',
-            subtle = '#${base03}',
-            text = '#${base05}',
-            love = '#${base09}',
-            gold = '#${base0A}',
-            rose = '#${base0B}',
-            pine = '#${base0C}',
-            foam = '#${base0D}',
-            iris = '#${base0E}',
-            highlight_low = '#${base01}',
-            highlight_med = '#${base03}',
-            highlight_high = '#${base06}',
-            opacity = 0.1,
-            none = 'NONE',
-        }
+  home.file.".config/nvim" = let
+    color-override-text = with config.banner.palette; ''
+      local palette = {
+          base = '#${base00}00',
+          surface = '#${base02}',
+          overlay = '#${base02}',
+          muted = '#${base00}',
+          subtle = '#${base03}',
+          text = '#${base05}',
+          love = '#${base09}',
+          gold = '#${base0A}',
+          rose = '#${base0B}',
+          pine = '#${base0C}',
+          foam = '#${base0D}',
+          iris = '#${base0E}',
+          highlight_low = '#${base01}',
+          highlight_med = '#${base03}',
+          highlight_high = '#${base06}',
+          opacity = 0.1,
+          none = 'NONE',
+      }
 
-        return palette
+      return palette
+    '';
+    color-override = builtins.toFile "color-override.lua" color-override-text;
+
+    overridenConfig = pkgs.stdenv.mkDerivation {
+      pname = "overriden-neovim-config";
+      version = "0.0.1";
+      src = nvim-config;
+      buildPhase = ''
+        cp ${color-override} lua/color-override.lua
+        ${pkgs.coreutils-full}/bin/chmod -R a+wr .
       '';
-      color-override = builtins.toFile "color-override.lua" text;
-
-      overridenConfig = pkgs.stdenv.mkDerivation {
-        pname = "overriden-neovim-config";
-        version = "0.0.1";
-        src = nvim-config;
-        buildPhase = ''
-          cp ${color-override} lua/color-override.lua
-          ${pkgs.coreutils-full}/bin/chmod -R a+wr .
-        '';
-        installPhase = "cp -r . $out";
-      };
-    in {
-      source = overridenConfig;
-      recursive = true;
+      installPhase = "cp -r . $out";
     };
+  in {
+    source = overridenConfig;
+    recursive = true;
   };
 }
