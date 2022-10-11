116ca1115f9459aa5d87ecf882b2d8b5838267f9
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-11 10:31:01 -0500

    add typing games

--- user/primary/default.nix
+++ user/primary/default.nix
@@ -133,6 +133,26 @@
         rnix-lsp
         libclang # clangd
 
+        # typing games
+        # thokr     # another simply game with the added ability to tweet
+        # your results
+        # ktouch    # underwhelming, QWERTY-only. provides a useful
+        # capitalization lesson for my bad shift habits
+        # ttyper    # simple game, just typing random words then it tells
+        # you your speed
+        # toipe     # another simple game, looks nice too
+
+        tipp10 # in-depth QWERTY trainer, could support dvorak with
+        # a bit of effort (key replacement filter)
+        klavaro # good for QWERTY, but only lets you do systemwide
+        # dvorak
+        gtypist # very good for qwerty, also requires systemwide
+        # dvorak
+        gotypist # makes you practice fast, slow, and medium.
+        # cool typing experiment, idk how good it is really
+        #typespeed  # words come flying at you from the left side. fun
+        # game, idk how good it really is for typing
+
         # appearance
         # rose-pine-gtk-theme
         # paper-gtk-theme # Paper
