55518aa4910c6d806f888e3e0dc627e570969479
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-10-14 16:58:40 -0400

    add back zsh-completions

--- user/primary/zsh.nix
+++ user/primary/zsh.nix
@@ -214,7 +214,7 @@ in {
     initExtra = with plugins; ''
       # PLUGINS----------------------------------------------------------------
       ${sandboxd.init}
-      # $\{zsh-completions.init}
+      ${zsh-completions.init}
       ${zsh-nix-shell.init}
       ${zsh-autopair.init}
       # INCLUDES---------------------------------------------------------------
