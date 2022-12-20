95643c2586837112d4a01307a8b3532f67facfd1
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-20 16:24:50 -0600

    remap qtile keys

--- modules/home-manager/desktops/qtile/config/config.py
+++ modules/home-manager/desktops/qtile/config/config.py
@@ -258,14 +258,14 @@ for i in groups:
             # mod1 + letter of group = switch to group
             Key(
                 [mod],
-                i.name,
+                remap(i.name),
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
-                i.name,
+                remap(i.name),
                 lazy.window.togroup(i.name, switch_group=True),
                 desc="Switch to & move focused window to group {}".format(i.name),
             ),
