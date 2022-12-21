f87e2212c5d089c7aff29c19a0bc01e77fce25dd
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-21 12:41:07 -0600

    update alias

--- user/primary/zsh.nix
+++ user/primary/zsh.nix
@@ -162,7 +162,7 @@ in {
         cageff = "cage \"/bin/firefox -p Unconfigured\"";
         awesomedoc = "firefox ${pkgs.awesome.doc}/share/doc/awesome/doc/index.html & disown";
         ix = "curl -F 'f:1=<-' ix.io";
-        org = "cd ~/NextcloudSync/org && nvim";
+        org = "cd ~/NextcloudSync/org && nvim refile.org";
       }
       // (pkgs.callPackage
         ../../lib/home-manager/xorg.nix
