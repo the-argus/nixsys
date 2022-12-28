fb6285e85150586418d0c4ee99e487811babf0d5
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-28 08:52:46 -0600

    dont patch

--- packages/awesome.nix
+++ packages/awesome.nix
@@ -6,4 +6,5 @@ unstable.awesome.overrideAttrs (_: {
     rev = "ee0663459922a41f57fa2cc936da80d5857eedc9";
     sha256 = "0wlhwabfm6mkkgf0s0f3nrm2masv6ma2g0xy24fa3z78vhwqxnib";
   };
+  dontPatch = true;
 })
