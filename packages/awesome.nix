40ebd792f647cc557632a36fd66b423ff8243468
Ian McFarlane <i.mcfarlane2002@gmail.com>
2022-12-28 08:35:24 -0600

    typo

--- packages/awesome.nix
+++ packages/awesome.nix
@@ -1,7 +1,7 @@
 {unstable, ...}:
 # basic override of awesome to use git master source code
 unstable.awesome.overrideAttrs (_: {
-  src = pkgs.fetchgit {
+  src = unstable.fetchgit {
     url = "https://github.com/awesomeWM/awesome";
     rev = "ee0663459922a41f57fa2cc936da80d5857eedc9";
     sha256 = "0wlhwabfm6mkkgf0s0f3nrm2masv6ma2g0xy24fa3z78vhwqxnib";
