561f7621a421ce69a1223af40a2687ed6e53cdce
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-10 16:11:18 -0600

    swap capslock and control

--- lib/home-manager/xorg.nix
+++ lib/home-manager/xorg.nix
@@ -54,6 +54,7 @@ in rec {
         "${pkgs.xfce.xfce4-clipman-plugin}/bin/xfce4-clipman"
         "${pkgs.xclip}/bin/xclip"
         "${picomPkg}/bin/picom --config ${picomConfigLocation} > ~/picom.log 2>&1"
+        "${pkgs.setxkbmap}/bin/xkbmap -option \"ctrl:swapcaps\""
 
         # restore feh wallpaper
         "$HOME/.fehbg"
