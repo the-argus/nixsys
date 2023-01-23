ac295cfaba5a244770264dde27a7671c4bf82f56
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-22 20:20:09 -0500

    remove dead code

--- modules/desktops/wayland.nix
+++ modules/desktops/wayland.nix
@@ -20,11 +20,8 @@ in {
       portal = {
         enable = true;
         wlr.enable = true;
-        # gtkUsePortal = true;
         extraPortals = with pkgs; [
           xdg-desktop-portal-wlr
-          # xdg-desktop-portal-gtk
-          #   xdg-desktop-portal-kde
         ];
       };
     };
