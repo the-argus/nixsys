9c5c746f29d6e95621554b62c9bec2e093a93e96
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-26 15:49:11 -0500

    update nvim-remote to handle special flatpak case

--- packages/neovim-remote.nix
+++ packages/neovim-remote.nix
@@ -6,14 +6,34 @@
 }:
 writeShellScriptBin "nvim-remote" ''
   term_exec=${terminal}/bin/${terminal.pname}
+  server_path=$HOME/.cache/nvim/nvim-remote.server.pipe
 
-  server_path=$HOME/.cache/nvim/godot.server.pipe
+  function open_file_server () {
+      if [ -e $server_path ]; then
+          # open file in server
+          nvim --server $server_path --remote "$@"
+      else
+          # start the server if its pipe doesn't exist
+          $term_exec -e nvim --listen $server_path "$@"
+      fi
+  }
 
-  if [ -e $server_path ]; then
-      # open file in server
-      nvim --server $server_path --remote "$@"
+  # unity flatpak call handling
+  IS_UNITY=
+  appended_str=""
+  for arg in $@; do
+      if [ "$arg" == "__UNITY_FLATPAK__" ]; then
+          IS_UNITY="yes"
+          continue
+      fi
+      appended_str="${appended_str} ${arg}"
+  done
+  # there's always a space at the front? get rid of that
+  appended_str=${appended_str:1}
+
+  if [ ! -z $IS_UNITY ]; then
+      open_file_server "$appended_str"
   else
-      # start the server if its pipe doesn't exist
-      $term_exec -e nvim --listen $server_path "$@"
+      open_file_server $@
   fi
 ''
