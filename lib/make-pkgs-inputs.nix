8dc1e5ab49a2900b5b3851edb02e9d2717af7e2f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-28 15:34:56 -0500

    correct wrapProgram syntax

--- lib/make-pkgs-inputs.nix
+++ lib/make-pkgs-inputs.nix
@@ -73,7 +73,7 @@ in {
             for binary in $src/bin/*; do
               ln -sf $binary $out/bin/$(${super.coreutils-full}/bin/basename $binary)
             done
-            wrapProgram --set PYTHONDONTWRITEBYTECODE "yes" $out/qtile
+            wrapProgram $out/qtile --set PYTHONDONTWRITEBYTECODE "yes"
           '';
         };
         gnome =
