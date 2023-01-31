e9e4043d210dd37947024236e57133d26ec77942
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-31 18:01:11 -0500

    add blugon to startup

--- lib/home-manager/xorg.nix
+++ lib/home-manager/xorg.nix
@@ -52,6 +52,8 @@ in rec {
 
         # restore feh wallpaper
         "$HOME/.fehbg"
+
+        "${pkgs.blugon}/bin/blugon"
       ]
       ++ (optionalList config.system.hardware.usesWireless wirelessAutostart)
       ++ (optionalList config.system.hardware.usesBluetooth bluetoothAutostart)
