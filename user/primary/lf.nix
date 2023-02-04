6186a1842ab00b3eb23a2b6b326aa838003e4df9
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-03 22:10:17 -0500

    sandboxed previews

--- user/primary/lf.nix
+++ user/primary/lf.nix
@@ -1,10 +1,43 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    # for pdftotext
-    poppler_utils
-    # for highlighting source text
-    highlight
-  ];
+{pkgs, ...}: let
+  cleaner = pkgs.writeShellScript "lf-cleaner.sh" ''
+    kitty +icat --clear --silent --transfer-mode file
+  '';
+
+  previewer = pkgs.writeShellScript "lf-previewer.sh" ''
+    case "$1" in
+      *.tar*) tar tf "$1";;
+      *.zip) unzip -l "$1";;
+      *.rar) unrar l "$1";;
+      *.7z) 7z l "$1";;
+      *.pdf) ${pkgs.poppler_utils}/bin/pdftotext "$1" -;;
+      *.md) ${pkgs.glow}/bin/glow "$1";;
+      *) ${pkgs.highlight}/bin/highlight -O ansi "$1";;
+    esac
+  '';
+
+  sandbox = pkgs.writeShellScript "lf-sandbox.sh" ''
+    # kitty previews
+    if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$1")" =~ ^image ]]; then
+      file=$1
+      w=$2
+      h=$3
+      x=$4
+      y=$5
+      kitty +icat --silent --transfer-mode file --place "${"\${w}x\${h}@\${x}x\${y}"}" "$file"
+      exit 1
+    fi
+    set -euo pipefail
+    (
+      exec ${pkgs.bubblewrap}/bin/bwrap \
+       --proc /proc \
+       --dev /dev  \
+       --ro-bind / / \
+       --unshare-all \
+       --new-session \
+       ${pkgs.runtimeShell} ${previewer} "$@"
+    )
+  '';
+in {
   programs.lf = {
     enable = true;
     settings = {
@@ -18,34 +51,10 @@
       tabstop = 2;
     };
     previewer = {
-      source = pkgs.writeShellScript "lf-previewer.sh" ''
-        # kitty previews
-        if [[ "$( ${pkgs.file}/bin/file -Lb --mime-type "$1")" =~ ^image ]]; then
-          file=$1
-          w=$2
-          h=$3
-          x=$4
-          y=$5
-          kitty +icat --silent --transfer-mode file --place "${"\${w}x\${h}@\${x}x\${y}"}" "$file"
-          exit 1
-        fi
-        case "$1" in
-          *.tar*) tar tf "$1";;
-          *.zip) unzip -l "$1";;
-          *.rar) unrar l "$1";;
-          *.7z) 7z l "$1";;
-          *.pdf) pdftotext "$1" -;;
-          *.md) glow "$1";;
-          *) highlight -O ansi "$1";;
-        esac
-      '';
+      source = sandbox;
       keybinding = "i";
     };
-    extraConfig = let
-      cleaner = pkgs.writeShellScript "lf-cleaner.sh" ''
-        kitty +icat --clear --silent --transfer-mode file
-      '';
-    in ''
+    extraConfig = ''
       set cleaner ${cleaner}
     '';
   };
