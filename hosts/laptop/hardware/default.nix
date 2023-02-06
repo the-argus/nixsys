adec14d7169bb1d6eccefda19b7fd6ba4627e49a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-06 10:28:19 -0500

    dont use ly, use gdm

--- hosts/laptop/hardware/default.nix
+++ hosts/laptop/hardware/default.nix
@@ -56,10 +56,10 @@
   # choose display manager
   # services.xserver.desktopManager.plasma5.enable = true;
   # services.xserver.displayManager.startx.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   # services.xserver.displayManager.sddm.enable = true;
   services.xserver.displayManager.ly = {
-    enable = true;
+    enable = false;
     defaultUser = username;
     package = pkgs.myPackages.ly;
     extraConfig = ''
