51e8dfff7a8266783119da38b907e44918ccb58f
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-02-28 12:43:04 -0500

    remove patching because askpass is fixed

--- packages/sudo-askpass/default.nix
+++ packages/sudo-askpass/default.nix
@@ -10,13 +10,9 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "Absolpega";
     repo = "simple-terminal-sudo-askpass";
-    rev = version;
-    sha256 = "sha256-pYlMEiVtBaApInkFtDz5tnhRiMnIq6YLY8tZb83gCig=";
+    rev = "aa0caa13801a19037af607016c0b719aaf97939d";
+    sha256 = "15wy59j5hxlin30nzm4kkx8jvd78pq0mccx6895b3f10khywhjxf";
   };
 
-  cargoSha256 = "sha256-tknAH0u+abVIshXxDkHxbzOfDkpL3gwCdiopdsIADtQ=";
-
-  prePatch = ''
-    sed -i "s/use std::os::fd::AsRawFd;/use std::os::unix::io::AsRawFd;/g" src/main.rs
-  '';
+  cargoSha256 = "sha256-AZpzhaNiWvve/XiFc+Ba/ZN7mG5DzbE8DZofXdq6mJ0=";
 }
