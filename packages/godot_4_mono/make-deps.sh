f70e39f58d261361aad94f4865d555686d56cd1b
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-03-29 15:22:45 -0400

    try manually running nuget-to-nix

--- packages/godot_4_mono/make-deps.sh
+++ packages/godot_4_mono/make-deps.sh
@@ -3,5 +3,4 @@ outdir=$PWD
 nix shell \
   --inputs-from ../.. \
   ../../.#myPackages.godot_4_mono.make-deps \
-  nixpkgs#nuget-to-nix \
-  --command nuget-to-nix nugetPackages > $outdir/deps.nix
+  nixpkgs#nuget-to-nix
