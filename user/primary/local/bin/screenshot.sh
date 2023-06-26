e73292b98eee3e9f420828d4ac1ee6326acc9a93
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-26 17:41:25 -0500

    remove geometry specifier

--- user/primary/local/bin/screenshot.sh
+++ user/primary/local/bin/screenshot.sh
@@ -3,7 +3,7 @@
 DATE=$(echo $(date +"%F_%T"))
 NAME=$(echo screenshot_$DATE)
 
-shotgun -g 1920x1080+0+0 -f png -s ~/Screenshots/$NAME.png
+shotgun -f png -s ~/Screenshots/$NAME.png
 
 # send notif
 dunstify -a "takeScreenshot" -u low -h string:x-dunst-stack-tag:"takeScreenshot" $NAME
