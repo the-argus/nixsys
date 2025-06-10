6ff84a984fd7324a0481ef04f6329ae0e6e90230
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-06-10 12:15:39 -0400

    horrible hacks to get custom keyboard layout

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -5,4 +5,7 @@
     ./dvorak.nix
     ../hardware.nix
   ];
+
+  xsession.enable = true;
+  home.file.".config/xkb/symbols/custom".source = ../../packages/patched_xkeyboardconfig/cycle-special-keys;
 }
