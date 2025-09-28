5bfff6f5407abf4dd878fc4cd0bc880f70bf8477
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-09-28 11:04:36 -0400

    add ptrace scope 0

--- hosts/pc/hardware/default.nix
+++ hosts/pc/hardware/default.nix
@@ -31,6 +31,7 @@
   };
 
   boot = {
+    kernel.sysctl."kernel.yama.ptrace_scope" = "0";
     #kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelPackages = pkgs.linuxPackages_zen;
     kernelParams = ["nordrand" "quiet" "systemd.show_status=0" "loglevel=4" "rd.systemd.show_status=auto" "rd.udev.log-priority=3" "rdblacklist=nouveau" "vfio-pci.ids=10de:1180,10de:0e0a"];
